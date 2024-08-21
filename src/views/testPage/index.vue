c0c4eec56fdec17df6605b02a4cca95cb7ab8938
aliyun4605180147 <luh15107060630@163.com>
2024-08-21 16:39:26 +0800

    feat:任务开始中断函数

--- src/views/testPage/index.vue
+++ src/views/testPage/index.vue
@@ -1,36 +1,58 @@
 <template>
   <div>
-    <canvas ref="canvas" width="200" height="200"></canvas>
+    <button @click="start">start</button>
+    <button @click="stop">stop</button>
   </div>
 </template>
 
 <script setup lang="ts">
-  const canvas = ref(null) // 定义canvas变量
+  const a = (list) => {
+    let flag = false
+    const taskResult = []
+    let i = 0
+    return {
+      startTask() {
+        return new Promise(async (resolve) => {
+          if (flag) return
+          flag = true
+          while (i < list.length) {
+            if (!flag) return
+            console.log(`startTask ${i} start`)
+            const result = await list[i]()
+            taskResult.push(result)
+            console.log(`startTask ${i} done`)
+            i++
+          }
+          resolve(taskResult)
+          flag = false
+        })
+      },
+      puaseTask() {
+        flag = false
+        console.log('stopTask')
+      },
+    }
+  }
+  const tasks: any[] = []
+  for (let i = 0; i < 5; i++) {
+    tasks.push(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(i)
+          }, 1000)
+        }),
+    )
+  }
+  const task = a(tasks)
 
-  onMounted(() => {
-    drawArrow()
-  })
-  const drawArrow = function () {
-    const ctx = canvas.value.getContext('2d') // 设置线条样式
-    ctx.strokeStyle = '#000' // 线条颜色
-    ctx.lineWidth = 2 // 线条宽度
-    // 绘制螺旋形箭头
-
-    ctx.beginPath()
-    ctx.moveTo(50, 150) // 起点
-    ctx.quadraticCurveTo(75, 130, 100, 100) // 第一段曲线
-    ctx.quadraticCurveTo(125, 100, 125, 75) // 第二段曲线
-    ctx.quadraticCurveTo(140, 75, 190, 30) // 第三段曲线
-    // 箭头
-
-    ctx.lineTo(180, 27) // 箭头上部分
-    ctx.moveTo(190, 30) // 回到箭头下部分起点
-    ctx.lineTo(188, 43) // 箭头下部分
-
-    ctx.stroke() // 完成绘制
-    // 设置透明背景（PNG导出时保持透明）
-
-    canvas.value.style.backgroundColor = 'transparent'
+  const start = () => {
+    task.startTask().then((res) => {
+      console.log(res)
+    })
+  }
+  const stop = () => {
+    task.puaseTask()
   }
 </script>
 
