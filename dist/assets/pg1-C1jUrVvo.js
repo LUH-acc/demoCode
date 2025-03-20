deea2816ce48b936622a926bea810bd71b0a3f3a
LUH-acc <207782041@qq.com>
2025-03-20 17:54:06 +0800

    feat:打包文件分类&解决github部署，部分文件无法获取问题

--- dist/assets/pg1-C1jUrVvo.js
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("pg1-C8mwWg31.jpg",import.meta.url).href;export{e as _};
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
 import VueJsx from '@vitejs/plugin-vue-jsx'
 
+// 若在vue中使用了异步组件（defineAsyncComponent），则最终构建产物中可能会出现名为_plugin-vue_export-helper.hash.js的文件。
+//  但部分web服务器无法处理以_开头的文件（如golang中的http.FileServer或Github Pages），这将导致响应404
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 // https://vitejs.dev/config/
 export default ({ command, mode }) => {
   // 获取环境变量
@@ -54,6 +59,31 @@ export default ({ command, mode }) => {
 
     build: {
       outDir: 'dist', // 指定打包路径，默认为项目根目录下的 dist 目录
+      rollupOptions: {
+        output: {
+          entryFileNames: `js/[name]-[hash].js`,
+          chunkFileNames: `js/[name]-[hash].js`,
+          assetFileNames(assetInfo) {
+            if(assetInfo.name.endsWith('.css')) {
+              return `css/[name]-[hash].css`
+            }
+            const imgTypes = ['.jpg','.jpeg','.png','.gif','.ico','.webp']
+
+            if(imgTypes.some((type) => assetInfo.name.endsWith(type))) {
+              return `images/[name]-[hash].[ext]`
+            }
+            return `assets/[name]-[hash].[ext]`
+          },
+          sanitizeFileName(fileName) {
+            const match = DRIVE_LETTER_REGEX.exec(fileName);
+            const driveLetter = match ? match[0] : "";
+            return (
+              driveLetter +
+              fileName.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+            );
+          },
+        }
+      },
       sourcemap: env.VITE_BUILD_SOURCEMAP === 'true',
       // minify默认esbuild，esbuild模式下terserOptions将失效
       // vite3变化：Terser 现在是一个可选依赖，如果你使用的是 build.minify: 'terser'，你需要手动安装它 `npm add -D terser`
